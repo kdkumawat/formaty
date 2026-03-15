712c55464cb892d2f4be403010b60cd6e32f04b9
kdkumawat <kumawat1993@gmail.com>
2026-03-15 11:23:05 +0530

    take 2

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
   transpilePackages: ["jsoncrack-react"],
+  webpack: (config) => {
+    config.resolve.conditionNames = [
+      "import",
+      ...(config.resolve.conditionNames ?? []),
+    ];
+    return config;
+  },
 };
 
 export default nextConfig;
